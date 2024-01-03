8c995c61df8257c1350fb9d98623f53020e11f2c
kappa_maintainer <gkappa@outlands.top>
2024-01-03 22:48:08 +0800

    Add patch to xaeroplus

--- src/main/java/com/cleanroommc/LateMixinLoader.java
+++ src/main/java/com/cleanroommc/LateMixinLoader.java
@@ -3,13 +3,14 @@ package com.cleanroommc;
 import net.minecraftforge.fml.common.Loader;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LateMixinLoader implements ILateMixinLoader {
     @Override
     public List<String> getMixinConfigs() {
-        return Collections.singletonList("fugue.mixin.charset.json");
+        return Arrays.asList("fugue.mixin.charset.json", "xaeroplus.mixin.charset.json");
     }
 
     @Override
@@ -17,6 +18,8 @@ public class LateMixinLoader implements ILateMixinLoader {
         switch (mixinConfig) {
             case "fugue.mixin.charset.json":
                 return Loader.isModLoaded("charset");
+            case "xaeroplus.mixin.charset.json":
+                return Loader.isModLoaded("xaeroplus");
             default:
                 return ILateMixinLoader.super.shouldMixinConfigQueue(mixinConfig);
                 
