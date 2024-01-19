1923e0e43e7f5e7c42fb7ebee12627419a4bdae5
kappa_maintainer <gkappa@outlands.top>
2024-01-19 15:43:20 +0800

    More cleanup

--- src/main/java/com/cleanroommc/mixin/aquaacrobatics/BlockFluidRendererMixin.java
+++
@@ -1,8 +0,0 @@
-package com.cleanroommc.mixin.aquaacrobatics;
-
-import net.minecraft.client.renderer.BlockFluidRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = BlockFluidRenderer.class, priority = 0)
-public class BlockFluidRendererMixin {
-}
