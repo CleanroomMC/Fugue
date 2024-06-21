f3355bb93e343aea5a0b01704938336ffa00d7da
arcade_kappa <gkappa@outlands.top>
2024-06-21 18:15:23 +0800

    More patches of exu2

---
+++ src/main/java/com/cleanroommc/fugue/mixin/extrautils2/TileIndexerMixin.java
@@ -0,0 +1,17 @@
+package com.cleanroommc.fugue.mixin.extrautils2;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.rwtema.extrautils2.transfernodes.TileIndexer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TileIndexer.class)
+public class TileIndexerMixin {
+    @Redirect(method = "reload", at = @At(value = "INVOKE", target = "Lcom/google/common/util/concurrent/Futures;addCallback(Lcom/google/common/util/concurrent/ListenableFuture;Lcom/google/common/util/concurrent/FutureCallback;)V"))
+    private void redirectAddCallback(ListenableFuture future, FutureCallback callback) {
+        Futures.addCallback(future, callback, Runnable::run);
+    }
+}
