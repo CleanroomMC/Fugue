453c81f8c158623b30fe3b918a5d881354be6359
arcade_kappa <gkappa@outlands.top>
2024-06-21 18:20:35 +0800

    Fix refmap

--- src/main/java/com/cleanroommc/fugue/mixin/extrautils2/TileIndexerMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/extrautils2/TileIndexerMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(TileIndexer.class)
+@Mixin(value = TileIndexer.class, remap = false)
 public class TileIndexerMixin {
     @Redirect(method = "reload", at = @At(value = "INVOKE", target = "Lcom/google/common/util/concurrent/Futures;addCallback(Lcom/google/common/util/concurrent/ListenableFuture;Lcom/google/common/util/concurrent/FutureCallback;)V"))
     private void redirectAddCallback(ListenableFuture future, FutureCallback callback) {
--- src/main/java/com/cleanroommc/fugue/mixin/extrautils2/TransferNodeEnergyMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/extrautils2/TransferNodeEnergyMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(TransferNodeEnergy.class)
+@Mixin(value = TransferNodeEnergy.class, remap = false)
 public class TransferNodeEnergyMixin {
     @Redirect(method = "update", at = @At(value = "INVOKE", target = "Lcom/google/common/util/concurrent/Futures;addCallback(Lcom/google/common/util/concurrent/ListenableFuture;Lcom/google/common/util/concurrent/FutureCallback;)V"))
     private void redirectAddCallback(ListenableFuture future, FutureCallback callback) {
