caa945acc673ad99f4917a463b353e8d7e1c861a
arcade_kappa <gkappa@outlands.top>
2024-08-27 09:22:50 +0800

    More LP fix

--- src/main/java/com/cleanroommc/fugue/transformer/logisticpipes/LogisticsPipesTrigger.java
+++ src/main/java/com/cleanroommc/fugue/transformer/logisticpipes/LogisticsPipesTrigger.java
@@ -12,6 +12,14 @@ public class LogisticsPipesTrigger implements IExplicitTransformer {
             Launch.classLoader.findClass("logisticspipes.network.packets.pipe.FluidSupplierMinMode");
             Launch.classLoader.findClass("logisticspipes.network.abstractguis.ModuleCoordinatesGuiProvider");
             Launch.classLoader.findClass("logisticspipes.network.guis.upgrade.SneakyUpgradeConfigGuiProvider");
+            Launch.classLoader.findClass("logisticspipes.network.packets.block.SecurityCardPacket");
+            Launch.classLoader.findClass("logisticspipes.network.packets.debug.PipeDebugLogAskForTarget");
+            Launch.classLoader.findClass("logisticspipes.network.packets.block.SecurityAuthorizationPacket");
+            Launch.classLoader.findClass("logisticspipes.network.packets.orderer.DiskDropPacket");
+            Launch.classLoader.findClass("logisticspipes.network.packets.AddNewChannelPacket");
+            Launch.classLoader.findClass("logisticspipes.network.packets.block.SecurityStationCCIDs");
+            Launch.classLoader.findClass("logisticspipes.network.guis.block.PowerProviderGui");
+            Launch.classLoader.findClass("logisticspipes.network.guis.module.inpipe.FluidSupplierSlot");
         } catch (ClassNotFoundException ignored) {}
         return bytes;
     }
