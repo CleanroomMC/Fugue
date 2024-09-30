8ae81c073cc1f98f62e2a01eaa9ca146ece2c5ab
arcade_kappa <gkappa@outlands.top>
2024-09-30 17:17:54 +0800

    Fix no refmap found on carry on patch

--- src/main/java/com/cleanroommc/fugue/mixin/carryon/ItemTileMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/carryon/ItemTileMixin.java
@@ -8,7 +8,7 @@ import tschipp.carryon.common.item.ItemTile;
 
 @Mixin(ItemTile.class)
 public class ItemTileMixin {
-    @Redirect(method = "onItemUse", at = @At(value = "FIELD", target = "Lcom/google/common/base/CharMatcher;JAVA_UPPER_CASE:Lcom/google/common/base/CharMatcher;"))
+    @Redirect(method = "onItemUse", at = @At(value = "FIELD", target = "Lcom/google/common/base/CharMatcher;JAVA_UPPER_CASE:Lcom/google/common/base/CharMatcher;", remap = false))
     private CharMatcher getCharMatcher() {
         return CharMatcher.javaUpperCase();
     }
