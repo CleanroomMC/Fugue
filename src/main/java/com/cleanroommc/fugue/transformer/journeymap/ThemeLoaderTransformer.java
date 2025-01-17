9acae4553474c7df1aa85244498b35d0f4315b32
arcade_kappa <gkappa@outlands.top>
2025-01-17 22:04:26 +0800

    Fix path to file casting

--- src/main/java/com/cleanroommc/fugue/transformer/journeymap/ThemeLoaderTransformer.java
+++ src/main/java/com/cleanroommc/fugue/transformer/journeymap/ThemeLoaderTransformer.java
@@ -4,6 +4,7 @@ import com.google.common.graph.Traverser;
 import com.google.common.io.MoreFiles;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.*;
 import top.outlands.foundation.IExplicitTransformer;
@@ -35,6 +36,7 @@ public class ThemeLoaderTransformer implements IExplicitTransformer {
                                 }
                                 if (methodInsnNode.name.equals("breadthFirstTraversal"))
                                 {
+                                    instructions.insertBefore(methodInsnNode, new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/File", "toPath", "()Ljava/nio/file/Path;", false));
                                     methodInsnNode.owner = Type.getType(Traverser.class).getInternalName();
                                     methodInsnNode.name = "breadthFirst";
                                     methodInsnNode.desc = "(Ljava/lang/Object;)Ljava/lang/Iterable;";
@@ -42,6 +44,13 @@ public class ThemeLoaderTransformer implements IExplicitTransformer {
                                 if (methodInsnNode.name.equals("iterator"))
                                 {
                                     instructions.remove(methodInsnNode);
+                                }
+                            }
+                            if (insnNode instanceof TypeInsnNode typeInsnNode)
+                            {
+                                if (typeInsnNode.getOpcode() == Opcodes.CHECKCAST) {
+                                    typeInsnNode.desc = "java/nio/file/Path";
+                                    instructions.insert(typeInsnNode, new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/nio/file/Path", "toFile", "()Ljava/io/File;", false));
                                     break out;
                                 }
                             }
