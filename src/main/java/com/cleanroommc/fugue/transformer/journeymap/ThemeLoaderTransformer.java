3181ed822bded530015c08f1032982bc0d042037
arcade_kappa <gkappa@outlands.top>
2025-01-18 09:12:32 +0800

    Fix jm patch * 2

--- src/main/java/com/cleanroommc/fugue/transformer/journeymap/ThemeLoaderTransformer.java
+++ src/main/java/com/cleanroommc/fugue/transformer/journeymap/ThemeLoaderTransformer.java
@@ -1,13 +1,13 @@
 package com.cleanroommc.fugue.transformer.journeymap;
 
-import com.google.common.graph.Traverser;
 import com.google.common.io.MoreFiles;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.*;
 import top.outlands.foundation.IExplicitTransformer;
+
+import java.io.File;
 
 public class ThemeLoaderTransformer implements IExplicitTransformer {
     @Override
@@ -30,27 +30,29 @@ public class ThemeLoaderTransformer implements IExplicitTransformer {
                             {
                                 if (methodInsnNode.name.equals("fileTreeTraverser"))
                                 {
-                                    methodInsnNode.owner = Type.getType(MoreFiles.class).getInternalName();
+                                    methodInsnNode.owner = "com/google/common/io/MoreFiles";
                                     methodInsnNode.name = "fileTraverser";
                                     methodInsnNode.desc = "()Lcom/google/common/graph/Traverser;";
                                 }
                                 if (methodInsnNode.name.equals("breadthFirstTraversal"))
                                 {
                                     instructions.insertBefore(methodInsnNode, new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/File", "toPath", "()Ljava/nio/file/Path;", false));
-                                    methodInsnNode.owner = Type.getType(Traverser.class).getInternalName();
+                                    methodInsnNode.owner = "com/google/common/graph/Traverser";
                                     methodInsnNode.name = "breadthFirst";
                                     methodInsnNode.desc = "(Ljava/lang/Object;)Ljava/lang/Iterable;";
                                 }
                                 if (methodInsnNode.name.equals("iterator"))
                                 {
-                                    instructions.remove(methodInsnNode);
+                                    methodInsnNode.setOpcode(Opcodes.INVOKEINTERFACE);
+                                    methodInsnNode.owner = "java/lang/Iterable";
+                                    methodInsnNode.itf = true;
                                 }
                             }
                             if (insnNode instanceof TypeInsnNode typeInsnNode)
                             {
                                 if (typeInsnNode.getOpcode() == Opcodes.CHECKCAST) {
                                     typeInsnNode.desc = "java/nio/file/Path";
-                                    instructions.insert(typeInsnNode, new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/nio/file/Path", "toFile", "()Ljava/io/File;", false));
+                                    instructions.insert(typeInsnNode, new MethodInsnNode(Opcodes.INVOKEINTERFACE, "java/nio/file/Path", "toFile", "()Ljava/io/File;", true));
                                     break out;
                                 }
                             }
@@ -60,7 +62,6 @@ public class ThemeLoaderTransformer implements IExplicitTransformer {
             }
         }
         ClassWriter classWriter = new ClassWriter(0);
-
         classNode.accept(classWriter);
         return classWriter.toByteArray();
     }
