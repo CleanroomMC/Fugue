a9fdb85e4c70604abbd2d1bcf95b4b8195651ac6
arcade_kappa <gkappa@outlands.top>
2025-07-14 21:53:19 +0800

    Fix refmap

--- src/main/java/com/cleanroommc/fugue/mixin/dissolution/PossessableEntityFactoryMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/dissolution/PossessableEntityFactoryMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(PossessableEntityFactory.class)
+@Mixin(value = PossessableEntityFactory.class, remap = false)
 public class PossessableEntityFactoryMixin {
     @Redirect(method = "lambda$defineGenericPossessable$0", at = @At(value = "INVOKE", target = "Lladysnake/dissolution/common/entity/PossessableEntityFactory$ASMClassLoader;define(Ljava/lang/String;[B)Ljava/lang/Class;"))
     private static Class<?> fixDefine(@Coerce ClassLoader instance, String name, byte[] data) {
