57329e2214a646ebe1e1532956902f2d517473c5
arcade_kappa <gkappa@outlands.top>
2026-01-18 22:10:11 +0800

    Fix xaero plus mixin no. 1

--- src/main/java/com/cleanroommc/fugue/mixin/xaeroplus/ChunkHighlightSavingCacheMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/xaeroplus/ChunkHighlightSavingCacheMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xaeroplus.util.highlights.ChunkHighlightSavingCache;
+import xaeroplus.feature.highlights.ChunkHighlightSavingCache;
 
 import java.util.List;
 
