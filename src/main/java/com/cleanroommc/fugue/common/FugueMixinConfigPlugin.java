a8a0a22dbf1bb470e4c1dc4a4b3d4eb8311202ac
arcade_kappa <gkappa@outlands.top>
2026-03-11 08:22:52 +0800

    Actually fix Recurrent Compled, close #122

--- src/main/java/com/cleanroommc/fugue/common/FugueMixinConfigPlugin.java
+++ src/main/java/com/cleanroommc/fugue/common/FugueMixinConfigPlugin.java
@@ -3,6 +3,8 @@ package com.cleanroommc.fugue.common;
 import com.cleanroommc.fugue.config.FugueConfig;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.common.Loader;
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -27,32 +29,53 @@ public class FugueMixinConfigPlugin implements IMixinConfigPlugin {
         return switch (mixinClassName.split("\\.")[4]) {
             case "charset" -> Loader.isModLoaded("charset") && FugueConfig.modPatchConfig.enableCharset;
             case "xaeroplus" -> Loader.isModLoaded("xaeroplus") && FugueConfig.modPatchConfig.enableXP;
-            case "codechickenlib" -> Launch.classLoader.isClassExist("codechicken.lib.CodeChickenLib") && !Launch.classLoader.isClassExist("codechicken.lib.Reference") && FugueConfig.modPatchConfig.enableCCL;
-            case "minecraftmultipartcbe" -> Loader.isModLoaded("minecraftmultipartcbe") && FugueConfig.modPatchConfig.enableMultiPart;
+            case "codechickenlib" ->
+                Launch.classLoader.isClassExist("codechicken.lib.CodeChickenLib")
+                        && !Launch.classLoader.isClassExist("codechicken.lib.Reference")
+                        && FugueConfig.modPatchConfig.enableCCL;
+            case "minecraftmultipartcbe" ->
+                Loader.isModLoaded("minecraftmultipartcbe") && FugueConfig.modPatchConfig.enableMultiPart;
             case "projectred_core" -> Loader.isModLoaded("projectred-core") && FugueConfig.modPatchConfig.enablePR;
             case "solarflux" -> Loader.isModLoaded("solarflux") && FugueConfig.modPatchConfig.enableSolarFlux;
             case "custommainmenu" -> Loader.isModLoaded("custommainmenu") && FugueConfig.modPatchConfig.enableCMM;
             case "hammercore" -> Loader.isModLoaded("hammercore") && FugueConfig.modPatchConfig.enableHammerCore;
-            case "gregtech" -> Loader.isModLoaded("gregtech") && FugueConfig.modPatchConfig.enableGTCEU && !Fugue.isModNewerThan("gregtech", "2.8.7");
+            case "gregtech" ->
+                Loader.isModLoaded("gregtech")
+                        && FugueConfig.modPatchConfig.enableGTCEU
+                        && !Fugue.isModNewerThan("gregtech", "2.8.7");
             case "mcjty" -> Loader.isModLoaded("mcjtylib_ng") && FugueConfig.modPatchConfig.enableMcjty;
             case "howlingmoon" -> Loader.isModLoaded("howlingmoon") && FugueConfig.modPatchConfig.enableHowlingMoon;
             case "customnpcs" -> Loader.isModLoaded("customnpcs") && FugueConfig.modPatchConfig.enableCustomNPC;
             case "waterpower" -> Loader.isModLoaded("waterpower") && FugueConfig.modPatchConfig.enableWaterPower;
             case "subaquatic" -> Loader.isModLoaded("subaquatic") && FugueConfig.modPatchConfig.enableSubaquatic;
-            case "armourers_workshop" -> Loader.isModLoaded("armourers_workshop") && FugueConfig.modPatchConfig.enableArmourersWorkshop;
+            case "armourers_workshop" ->
+                Loader.isModLoaded("armourers_workshop") && FugueConfig.modPatchConfig.enableArmourersWorkshop;
             case "mage" -> Loader.isModLoaded("mage") && FugueConfig.modPatchConfig.enableMage;
             case "extrautils2" -> Loader.isModLoaded("extrautils2") && FugueConfig.modPatchConfig.enableExtraUtilities;
-            case "refinedstorage" -> Loader.isModLoaded("refinedstorage") && FugueConfig.modPatchConfig.enableMoreRefinedStorage && Fugue.isModNewerThan("refinedstorage", "2.0.0");
-            case "jei" -> Loader.isModLoaded("jei") && FugueConfig.modPatchConfig.enableHEI && Fugue.isModNewerThan("jei", "4.17.0");
+            case "refinedstorage" ->
+                Loader.isModLoaded("refinedstorage")
+                        && FugueConfig.modPatchConfig.enableMoreRefinedStorage
+                        && Fugue.isModNewerThan("refinedstorage", "2.0.0");
+            case "jei" ->
+                Loader.isModLoaded("jei")
+                        && FugueConfig.modPatchConfig.enableHEI
+                        && Fugue.isModNewerThan("jei", "4.17.0");
             case "carryon" -> Loader.isModLoaded("carryon") && FugueConfig.modPatchConfig.enableCarryon;
-            case "betterrecords" -> Loader.isModLoaded("betterrecords") && FugueConfig.modPatchConfig.enableBetterRecords;
+            case "betterrecords" ->
+                Loader.isModLoaded("betterrecords") && FugueConfig.modPatchConfig.enableBetterRecords;
             case "unilib" -> Loader.isModLoaded("unilib") && FugueConfig.modPatchConfig.enableUnilib;
-            case "funkylocomotion" -> Loader.isModLoaded("funkylocomotion") && FugueConfig.modPatchConfig.enableFunkyLocomotion;
+            case "funkylocomotion" ->
+                Loader.isModLoaded("funkylocomotion") && FugueConfig.modPatchConfig.enableFunkyLocomotion;
             case "patchouli" -> Loader.isModLoaded("patchouli") && FugueConfig.modPatchConfig.enablePatchouli;
-            case "reccomplex" -> Loader.isModLoaded("reccomplex") && FugueConfig.modPatchConfig.enableRecurrentComplex;
+            case "reccomplex" ->
+                Loader.isModLoaded("reccomplex")
+                        && FugueConfig.modPatchConfig.enableRecurrentComplex
+                        && (!mixinClassName.equals("com.cleanroommc.fugue.mixin.reccomplex.ParametersMixin")
+                                || SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_22));
             case "lockdown" -> Loader.isModLoaded("lockdown") && FugueConfig.modPatchConfig.enableLockdown;
             case "dissolution" -> Loader.isModLoaded("dissolution") && FugueConfig.modPatchConfig.enableDissolution;
-            case "glibysphysics" -> Loader.isModLoaded("glibysphysics") && FugueConfig.modPatchConfig.enableGlibysPhysics;
+            case "glibysphysics" ->
+                Loader.isModLoaded("glibysphysics") && FugueConfig.modPatchConfig.enableGlibysPhysics;
             case "darktheme" -> Loader.isModLoaded("darktheme") && FugueConfig.modPatchConfig.enableDarkTheme;
             default -> true;
         };
--- src/main/java/com/cleanroommc/fugue/mixin/reccomplex/ParametersMixin.java
+++ src/main/java/com/cleanroommc/fugue/mixin/reccomplex/ParametersMixin.java
@@ -1,7 +1,9 @@
 package com.cleanroommc.fugue.mixin.reccomplex;
 
 import ivorius.reccomplex.shadow.mcopts.commands.parameters.Parameters;
+import net.lenni0451.reflect.Fields;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,26 +13,17 @@ import java.lang.reflect.Field;
 
 @Mixin(Parameters.class)
 public class ParametersMixin {
+    @Unique
+    private static Field fugue$r = null;
+    @Unique
+    private static Field fugue$next = null;
+
     @Inject(method = "index", at = @At("HEAD"), cancellable = true)
     private static void index(StringReader reader, CallbackInfoReturnable<Integer> cir) {
-        Class<?> clazz = reader.getClass();
-        Field next = null;
-        while (clazz != Object.class && next == null) {
-            try {
-                next = clazz.getDeclaredField("next");
-                next.setAccessible(true);
-            } catch (NoSuchFieldException e) {
-                clazz = clazz.getSuperclass();
-            }
+        if (fugue$next == null) {
+            fugue$r = Fields.getDeclaredField(StringReader.class, "r");
+            fugue$next = Fields.getDeclaredField(Fields.get(reader, fugue$r).getClass(), "next");
         }
-        if (next != null) {
-            try {
-                cir.setReturnValue(next.getInt(reader));
-            } catch (IllegalAccessException e) {
-                cir.setReturnValue(0);
-            }
-        } else {
-            cir.setReturnValue(0);
-        }
+        cir.setReturnValue(Fields.get(Fields.get(reader, fugue$r), fugue$next));
     }
 }
